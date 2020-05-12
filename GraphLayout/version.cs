d8f14ed1787d7ed733cfb3053fd3be43003b3f56
Laszlo Frank <laszlo.frank@gmail.com>
2020-05-12 17:30:51 +0200

    Catch exception in GViewer layoutthread

--- GraphLayout/version.cs
+++ GraphLayout/version.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
 #if !RAZZLE
-[assembly:AssemblyVersion("3.0.0.10")]
-[assembly: AssemblyFileVersion("3.0.0.10")]
+[assembly:AssemblyVersion("3.0.0.11")]
+[assembly: AssemblyFileVersion("3.0.0.11")]
 #endif
